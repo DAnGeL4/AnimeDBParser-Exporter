c935039b3bc08a089a8009b61f398c4d6f710333
DAnGeL4 <Lazukad@mail.ru>
2023-01-22 11:15:00 +0000

    Added function 'receive_data' for 'data_rcv' flask route.

--- modules/flask/application.py
+++ modules/flask/application.py
@@ -1,6 +1,7 @@
 #--Start imports block
 #System imports
 import os
+import time
 import typing as typ
 from flask import Flask, jsonify
 from flask_session import Session
@@ -23,9 +24,11 @@ def index() -> cfg.WebPage:
     '''
     Returns the primary web page.
     '''
-    _ = app_service.SessionService.check_common_keys()
+    ss_srv = app_service.SessionService()
     render_srv = app_service.HTMLRenderingService()
     dt_srv = app_service.DataService()
+
+    _ = ss_srv.check_common_keys()
 
     data = dict({
         'settings': dt_srv.get_modules_settings(),
@@ -43,15 +46,15 @@ def settingup() -> cfg.JSON:
     Works only with the post method.
     '''
     req_srv = app_service.RequestService()
-    
+
     if req_srv.method_is_post():
         module = req_srv.get_passed_module()
         selected_module = req_srv.get_passed_selected_module()
         cookies = req_srv.get_passed_cookies()
-        
+
         ss_srv = app_service.SessionService(module=module)
         _ = ss_srv.set_filled_settings(selected_module, cookies)
-        
+
         response_obj = cfg.AjaxServerResponse()
         render_srv = app_service.HTMLRenderingService()
         
@@ -71,7 +74,7 @@ def action() -> cfg.JSON:
     cmd_srv = app_service.CommandService()
     req_srv = app_service.RequestService()
     dt_srv = app_service.DataService()
-    
+
     if req_srv.method_is_post():
         action = req_srv.get_passed_action()
         cmd = req_srv.get_passed_command()
@@ -79,17 +82,37 @@ def action() -> cfg.JSON:
         module = req_srv.get_module_by_action(action)
 
         if not dt_srv.args_is_empty([action, cmd, optional_args, module]):
-            ss_srv = app_service.SessionService(module=module)
-            _ = ss_srv.check_ask_cmd_keys()
             _ = cmd_srv.init_args(action, module, optional_args)
         else:
             cmd = cfg.AjaxCommand.DEFAULT
-            
+
         _ = cmd_srv.run_command(cmd)
 
     return jsonify(cmd_srv.get_response())
 
+@app.route('/data_rcv', methods=["POST", "GET"])
+def receive_data() -> typ.NoReturn:
+    '''
+    Accepts the states passed before the page was reloaded.
+    '''
+    req_srv = app_service.RequestService()
+    ss_srv = app_service.SessionService()
+    response_obj = cfg.AjaxServerResponse()
+    
+    response_obj.status = cfg.ResponseStatus.FAIL
 
+    if req_srv.method_is_post():
+        data = req_srv.get_passed_selects()
+        module = req_srv.title_tab_module_compatibility[data['slct_pill']]
+
+        ss_srv.set_selected_dropdown_tab(cfg.ActionModule.PARSER, data['slct_parsed_drpdwn'])
+        ss_srv.set_selected_dropdown_tab(cfg.ActionModule.EXPORTER, data['slct_exported_drpdwn'])
+        ss_srv.session_selected_setting_tab = module
+        
+        response_obj.status = cfg.ResponseStatus.DONE
+
+    return jsonify(response_obj.asdict())
+    
 #--Finish functional block
 
 
@@ -102,6 +125,6 @@ def run_app() -> typ.NoReturn:
     app.config['SESSION_TYPE'] = 'filesystem'
 
     sess.init_app(app)
-    
+
     app.run(host='0.0.0.0', port=8080)
 #--Finish main block
