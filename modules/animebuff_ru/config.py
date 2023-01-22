3ede7d3db67881537fc3279826ea4155ba702270
DAnGeL4 <Lazukad@mail.ru>
2023-01-22 10:49:00 +0000

    Refactored. Corrected type names.

--- modules/animebuff_ru/config.py
+++ modules/animebuff_ru/config.py
@@ -30,15 +30,15 @@ class AnimeBuffRuConfig(ac.SiteSettings):
     url_type_option = "?type="
 
     url_types = {
-        cfg.WatchListTypes.WATCH: 
+        cfg.WatchListType.WATCH: 
             "%D0%A1%D0%BC%D0%BE%D1%82%D1%80%D1%8E",
-        cfg.WatchListTypes.DESIRED: 
+        cfg.WatchListType.DESIRED: 
             "%D0%91%D1%83%D0%B4%D1%83%20%D1%81%D0%BC%D0%BE%D1%82%D1%80%D0%B5%D1%82%D1%8C",
-        cfg.WatchListTypes.VIEWED: 
+        cfg.WatchListType.VIEWED: 
             "%D0%9F%D1%80%D0%BE%D1%81%D0%BC%D0%BE%D1%82%D1%80%D0%B5%D0%BD%D0%BE",
-        cfg.WatchListTypes.ABANDONE: 
+        cfg.WatchListType.ABANDONE: 
             "%D0%97%D0%B0%D0%B1%D1%80%D0%BE%D1%88%D0%B5%D0%BD%D0%BE",
-        cfg.WatchListTypes.FAVORITES: 
+        cfg.WatchListType.FAVORITES: 
             "%D0%98%D0%B7%D0%B1%D1%80%D0%B0%D0%BD%D0%BD%D0%BE%D0%B5"
     }
 
