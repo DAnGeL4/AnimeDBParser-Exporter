4fb7eda2fcc34242000c5f4ea668921541a6522e
DAnGeL4 <Lazukad@mail.ru>
2023-02-04 11:08:00 +0000

    Application objects are moved to a separate file.

---
+++ modules/common/application_objects.py
@@ -0,0 +1,27 @@
+#--Start imports block
+#System imports
+from flask import Flask
+from flask_session import Session
+from flask_caching import Cache
+from celery import Celery
+
+#Custom imports
+from lib.factory import FlaskFactory, CeleryFactory
+from configs.settings import FLASK_SESSION_FILE_DIR, FLASK_CACHE_DIR
+from configs.settings import CELERY_RESULT_BACKEND, CELERY_BROKER_URL
+from configs.settings import FLASK_APPLICATION_NAME
+#--Finish imports block
+
+
+#--Start global constants block
+flask_app: Flask = FlaskFactory.make_flask_application(
+    name=FLASK_APPLICATION_NAME,
+    session_dir=FLASK_SESSION_FILE_DIR,
+    cache_dir=FLASK_CACHE_DIR)
+celery: Celery = CeleryFactory.make_celery(
+    app=flask_app,
+    backend=CELERY_RESULT_BACKEND,
+    broker=CELERY_BROKER_URL)
+flask_sess: Session = FlaskFactory.make_flask_session(flask_app)
+flask_cache: Cache = FlaskFactory.make_flask_cache(flask_app)
+#--Finish global constants block
