468930d9d4d06e6906232c89bd195b61d56fd22b
DAnGeL4 <Lazukad@mail.ru>
2023-02-05 10:19:00 +0000

    Added a script for the initial configuration of application components.

---
+++ manual_setup.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# configuring Redis
+#---
+# donloading config file
+curl -O https://raw.githubusercontent.com/redis/redis/6.2/redis.conf
+wait $!
+mv redis.conf "$(pwd)/configs"
+
+# starting Redis
+(redis-server ./configs/redis.conf --bind 127.0.0.1 &)
+for (( i=0; i<5; ++i )); do
+    if [ "$(redis-cli ping 2>/dev/null)" = "PONG" ]; then break; fi
+    sleep 1;
+done
+
+# changing configuration
+redis-cli CONFIG SET dir "$(pwd)/var/db_dumps"
+redis-cli CONFIG SET "dbfilename" "redis_dump.rdb"
+redis-cli CONFIG REWRITE
+redis-cli SHUTDOWN SAVE
+
+# making an executable Redis startup script
+chmod u+x "$(pwd)/sh_scripts/redis_up.sh"
+#---
