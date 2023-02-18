e03f10f8c5746a81011fe9db153f61f9c4b2bc41
DAnGeL4 <Lazukad@mail.ru>
2023-02-18 10:52:00 +0000

    Refactored. Redesigned 'settingup' stub.

--- modules/flask/application.py
+++ modules/flask/application.py
@@ -1,25 +1,23 @@
 #--Start imports block
 #System imports
-import os
-import time
 import typing as typ
-from flask import Flask, jsonify
-from flask_session import Session
+from flask import jsonify
 
 #Custom imports
 from configs import settings as cfg
+from configs.application_objects import flask_app, flask_cache
 from modules.flask.routes import routes
 from modules.flask import service as app_service
+from modules.general.main_tools import MainService
 #--Finish imports block
 
+
 #--Start global constants block
-app = Flask('app')
-sess = Session(app)
 #--Finish global constants block
 
 
 #--Start functional block
-@app.route(routes['index'])
+@flask_app.route(routes['index'])
 def index() -> cfg.WebPage:
     '''
     Returns the primary web page.
@@ -38,34 +36,49 @@ def index() -> cfg.WebPage:
 
     return render_srv.get_initial_page(data)
 
-
-@app.route(routes['settingup'], methods=["POST", "GET"])
+@flask_app.route(routes['settingup'], methods=["POST", "GET"])
 def settingup() -> cfg.JSON:
     '''
     Receives data for authorization of the user on the selected platform.
     Works only with the post method.
     '''
     req_srv = app_service.RequestService()
-
     if req_srv.method_is_post():
+        response = req_srv.set_response(
+            cfg.ResponseStatus.DONE, 'User authorized.')
+        
         module = req_srv.get_passed_module()
         selected_module = req_srv.get_passed_selected_module()
         cookies = req_srv.get_passed_cookies()
 
-        ss_srv = app_service.SessionService(module=module)
-        _ = ss_srv.set_filled_settings(selected_module, cookies)
+        dt_srv = app_service.DataService()
+        if dt_srv.args_is_empty([module, selected_module, cookies]):
+            response = req_srv.set_response(
+                cfg.ResponseStatus.FAIL, 'Incorrect data.')
 
-        response_obj = cfg.AjaxServerResponse()
-        render_srv = app_service.HTMLRenderingService()
-        
-        response_obj.status = cfg.ResponseStatus.FAIL
-        response_obj.msg = render_srv.get_rendered_alert(
-            cfg.ResponseStatus.FAIL, 'The user is not authorized.')
+        if response.status is not cfg.ResponseStatus.FAIL:
+            selected_module = selected_module(cookies=cookies)
+            
+            m_serv = MainService()
+            ss_srv = app_service.SessionService(module=module)
+            _ = ss_srv.set_filled_settings(selected_module, cookies)
+            
+            res = m_serv.prepare_module(module=selected_module,
+                                        module_name=selected_module.module_name)
+            if res:
+                _ = flask_cache.set(module.value, selected_module)
+                _ = ss_srv.session_username = selected_module.config_module.username
+            else:
+                response = req_srv.set_response(
+                    cfg.ResponseStatus.FAIL, 'The user is not authorized.')
+            
+    else:
+        response = req_srv.set_response(
+            cfg.ResponseStatus.FAIL, 'Incorrect method.')
 
-    return jsonify(response_obj.asdict())
+    return jsonify(response.asdict())
 
-
-@app.route(routes['action'], methods=["POST", "GET"])
+@flask_app.route(routes['action'], methods=["POST", "GET"])
 def action() -> cfg.JSON:
     '''
     Runes the selected command for the selected action on the server. 
@@ -90,7 +103,7 @@ def action() -> cfg.JSON:
 
     return jsonify(cmd_srv.get_response())
 
-@app.route('/data_rcv', methods=["POST", "GET"])
+@flask_app.route('/data_rcv', methods=["POST", "GET"])
 def receive_data() -> typ.NoReturn:
     '''
     Accepts the states passed before the page was reloaded.
@@ -105,8 +118,10 @@ def receive_data() -> typ.NoReturn:
         data = req_srv.get_passed_selects()
         module = req_srv.title_tab_module_compatibility[data['slct_pill']]
 
-        ss_srv.set_selected_dropdown_tab(cfg.ActionModule.PARSER, data['slct_parsed_drpdwn'])
-        ss_srv.set_selected_dropdown_tab(cfg.ActionModule.EXPORTER, data['slct_exported_drpdwn'])
+        ss_srv.set_selected_dropdown_tab(cfg.ActionModule.PARSER, 
+                                         data['slct_parsed_drpdwn'])
+        ss_srv.set_selected_dropdown_tab(cfg.ActionModule.EXPORTER, 
+                                         data['slct_exported_drpdwn'])
         ss_srv.session_selected_setting_tab = module
         
         response_obj.status = cfg.ResponseStatus.DONE
@@ -119,12 +134,10 @@ def receive_data() -> typ.NoReturn:
 #--Start main block
 def run_app() -> typ.NoReturn:
     '''
+    Entry point in the flask application.
     Runs the flask application.
     '''
-    app.secret_key = os.environ['flask_secret_key']
-    app.config['SESSION_TYPE'] = 'filesystem'
+    
+    flask_app.run(host='0.0.0.0', port=8080)
 
-    sess.init_app(app)
-
-    app.run(host='0.0.0.0', port=8080)
 #--Finish main block
