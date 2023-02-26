9f5d7430f68430b18f15a194b255f81d453862d6
DAnGeL4 <Lazukad@mail.ru>
2023-02-26 08:35:00 +0000

    Added new settings.

--- modules/common/application_objects.py
+++ modules/common/application_objects.py
@@ -6,10 +6,12 @@ from flask_caching import Cache
 from celery import Celery
 
 #Custom imports
+from configs.settings import (
+    FLASK_SESSION_FILE_DIR, FLASK_CACHE_DIR, 
+    CELERY_RESULT_BACKEND, CELERY_BROKER_URL, 
+    FLASK_APPLICATION_NAME, FLASK_SESSION_FILE_THRESHOLD
+)
 from lib.factory import FlaskFactory, CeleryFactory
-from configs.settings import FLASK_SESSION_FILE_DIR, FLASK_CACHE_DIR
-from configs.settings import CELERY_RESULT_BACKEND, CELERY_BROKER_URL
-from configs.settings import FLASK_APPLICATION_NAME
 #--Finish imports block
 
 
@@ -17,11 +19,14 @@ from configs.settings import FLASK_APPLICATION_NAME
 flask_app: Flask = FlaskFactory.make_flask_application(
     name=FLASK_APPLICATION_NAME,
     session_dir=FLASK_SESSION_FILE_DIR,
-    cache_dir=FLASK_CACHE_DIR)
+    cache_dir=FLASK_CACHE_DIR,
+    session_thresh=FLASK_SESSION_FILE_THRESHOLD
+)
 celery: Celery = CeleryFactory.make_celery(
     app=flask_app,
     backend=CELERY_RESULT_BACKEND,
-    broker=CELERY_BROKER_URL)
+    broker=CELERY_BROKER_URL
+)
 flask_sess: Session = FlaskFactory.make_flask_session(flask_app)
 flask_cache: Cache = FlaskFactory.make_flask_cache(flask_app)
 #--Finish global constants block
