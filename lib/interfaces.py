e688f8bbfb5112f7fff4b31c51d4f56a1e7a9552
DAnGeL4 <Lazukad@mail.ru>
2023-03-27 10:30:00 +0000

    Added progress handlers interface.

--- lib/interfaces.py
+++ lib/interfaces.py
@@ -4,6 +4,8 @@ import os
 import re
 import json
 import typing as typ
+from billiard import Queue
+from pathlib import Path
 from bs4 import BeautifulSoup
 from pydantic import AnyHttpUrl, Protocol
 from requests.structures import CaseInsensitiveDict
@@ -13,7 +15,7 @@ from logging import Logger
 from configs.settings import (
     WebPage, AnimeInfoType, LinkedAnimeInfoType, 
     WatchListType, AnimeType, AnimeStatuse, 
-    JSON, Cookies
+    JSON, Cookies, ServerAction, TitlesProgressStatus
 )
 #--Finish imports block
 
@@ -101,6 +103,9 @@ class IWebPageParser:
     Abstract class describing 
     the necessary methods and attributes.
     '''
+    _queue: Queue
+    _logger: Logger
+    _url_general: AnyHttpUrl
     
     def __init__(self, parser_module: 'IWebPageParser') -> typ.NoReturn:
         self._url_general: AnyHttpUrl
@@ -176,6 +181,15 @@ class IConnectedModule:
     def __init__(self, cookies: typ.Union[str, Cookies, JSON]=None):
         self.config_module = self.config_module(cookies)
 
+    def get_json_dump_name(self) -> typ.Union[str, Path]:
+        '''
+        Returns the name of the dump file for a specific user.
+        Used by the file data handler.
+        '''            
+        file_name = f"{self.config_module.user_num}_{self.json_dump_name}"
+        json_dump_name = Path(f"{self.module_name}/{file_name}")
+        return json_dump_name
+
 
 class IDataHandler(dict):
     '''
@@ -203,6 +217,71 @@ class IDataHandler(dict):
         '''
         Stores data in the destination store.
         Storage is specified in the implementation.
+        '''
+        pass
+
+
+class IProgressHandler:
+    '''
+    Contains methods for working with progress data 
+    for a progress bar template.
+    '''
+    _action: ServerAction
+    _storage: object
+
+    def __init__(self, storage_srv: object, action: ServerAction):
+        self._action = action
+        self._storage = storage_srv
+        
+    @property
+    def progress(self) -> typ.Union[TitlesProgressStatus, None]:
+        '''
+        Returns progress data from the storage by action.
+        '''
+        pass
+        
+    @progress.setter
+    def progress(self, progress: TitlesProgressStatus) -> typ.NoReturn:
+        '''
+        Stores progress data in storage by action.
+        '''
+        pass
+
+    def initialize_progress(self, status: bool = False,
+                            n_now: int = 0, n_max: int = 0) -> typ.NoReturn:
+        '''
+        Clears progress data from storage 
+        and reinitializes new ones.
+        The current progress is set by default.
+        '''
+        pass
+
+    def initialize_progress_curr(self, watch_list: WatchListType,
+                                   n_now: int = 0, n_max: int = 0) -> typ.NoReturn:
+        '''
+        Initializes the current progress data 
+        and writes to the common progress data in the storage.
+        '''
+        pass
+
+    def increase_progress_all(self) -> typ.NoReturn:
+        '''
+        Increases the total number of processed titles.
+        '''
+        pass
+
+    def increase_progress_curr(self, inc_prgs_all: bool = True) -> typ.NoReturn:
+        '''
+        Increases the number of processed titles 
+        in the current processing. 
+        Increases the total number 
+        of titles processed, if specified.
+        '''
+        pass
+
+    def switch_status(self, status: bool = None) -> typ.NoReturn:
+        '''
+        Toggles the status to the opposite if not specified.
         '''
         pass
 
