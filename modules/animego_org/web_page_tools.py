14d440e15541bec393b26ce541b0c48b802737b7
DAnGeL4 <Lazukad@mail.ru>
2023-04-15 10:26:00 +0000

    Added parsing all titles count.

--- modules/animego_org/web_page_tools.py
+++ modules/animego_org/web_page_tools.py
@@ -23,6 +23,7 @@ class WebPageParser(IWebPageParser):
     the anime information from AnimeGo.org site 
     and its transformation to a unified state.
     '''
+    _list_all_tag = "Все"
     _types_compatibility = {
         'tv': AnimeType.TV.value,
         'movie': AnimeType.MOVIE.value,
@@ -47,50 +48,6 @@ class WebPageParser(IWebPageParser):
                                     queue=self._queue, 
                                     name='parser_animego_org').logger
         self._url_general = module_url_general
-        
-    def parse_original_name_from_card(self, item: BeautifulSoup) -> str:
-        '''
-        Parses anime original name from the card item.
-        '''
-        item_original_name = item.find(class_='text-gray-dark-6')
-        title_original_name = item_original_name.text.strip()
-        return title_original_name
-    
-    def parse_name_from_card(self, item: BeautifulSoup) -> str:
-        '''
-        Parses anime name from the card item.
-        '''
-        item_name = item.find(class_='card-title')
-        item_anime_link = item_name.find('a')
-        title_name = item_anime_link.get('title')
-        return title_name
-        
-    def parse_link_from_card(self, item: BeautifulSoup) -> AnyHttpUrl:
-        '''
-        Parses anime link from the card item.
-        '''
-        item_name = item.find(class_='card-title')
-        item_anime_link = item_name.find('a')
-        title_link = item_anime_link.get('href')
-        return title_link
-        
-    def parse_type_from_card(self, item: BeautifulSoup) -> AnimeType:
-        '''
-        Parses anime type from the card item.
-        '''
-        item_type_year = item.find(class_='animes-grid-item-body-info')
-        item_type = item_type_year.find(class_='text-link-gray').get('href')
-        title_type = item_type.split('/')[-1]
-        return self._types_compatibility[title_type]
-        
-    def parse_year_from_card(self, item: BeautifulSoup) -> int:
-        '''
-        Parses anime year from the card item.
-        '''
-        item_type_year = item.find(class_='animes-grid-item-body-info')
-        item_year = item_type_year.find(class_='anime-year').find('a').get('href')
-        title_year = item_year.split('/')[-1]
-        return title_year
     
     def get_parsed_titles(self, items_list: typ.List[BeautifulSoup]
                          ) -> typ.List[LinkedAnimeInfoType]:
@@ -102,14 +59,14 @@ class WebPageParser(IWebPageParser):
         for item in items_list:
             info_obj = LinkedAnimeInfoType(
                 poster=None,
-                name=self.parse_name_from_card(item),
-                original_name=self.parse_original_name_from_card(item),
+                name=self._parse_name_from_card(item),
+                original_name=self._parse_original_name_from_card(item),
                 other_names=list(),
-                type=self.parse_type_from_card(item),
+                type=self._parse_type_from_card(item),
                 ep_count=None,
-                year=self.parse_year_from_card(item),
+                year=self._parse_year_from_card(item),
                 status=None,
-                link=self.parse_link_from_card(item)
+                link=self._parse_link_from_card(item)
             )
             parsed_titles.append(info_obj)
             
@@ -121,12 +78,12 @@ class WebPageParser(IWebPageParser):
         '''
         soup = BeautifulSoup(web_page, 'lxml')
         
+        items_anime_cards = None
         try:    
             item_amime_list = soup.find(class_='animes-grid')
             items_anime_cards = item_amime_list.find_all(class_='animes-grid-item-body')
-            
         except:
-            return list()
+            items_anime_cards = list()
             
         return items_anime_cards
 
@@ -135,22 +92,102 @@ class WebPageParser(IWebPageParser):
         '''
         Prepares the necessary link to the action 
         on the anime web page.
-        '''
+        '''     
         self._logger.info("Looking for the action link...")
+                              
+        item_action = None
         action = self._actions_compatibility[action]
-        if not action: 
+                              
+        if action: 
+            soup = BeautifulSoup(web_page, 'lxml')
+            item_list_actions = soup.find(class_='my-list')
+            item_action = item_list_actions.find('span', class_='list-group-item', 
+                                                 string=action)
+        else:
             self._logger.error("Unknown action type.")
-            return
-            
-        soup = BeautifulSoup(web_page, 'lxml')
-        item_list_actions = soup.find(class_='my-list')
-        item_action = item_list_actions.find('span', class_='list-group-item', 
-                                             string=action)
-        if not item_action:
+
+        action_link = None
+        if item_action:
+            action_link = item_action.get('data-ajax-url')
+            self._logger.info("...action link finded.")
+        else:
             self._logger.warning("The requested action is not active.")
-            return
             
-        action_link = item_action.get('data-ajax-url')
-        self._logger.info("...action link finded.")
         return action_link
+        
+    def _parse_original_name_from_card(self, item: BeautifulSoup) -> str:
+        '''
+        Parses anime original name from the card item.
+        '''
+        item_original_name = item.find(class_='text-gray-dark-6')
+        title_original_name = item_original_name.text.strip()
+        return title_original_name
+    
+    def _parse_name_from_card(self, item: BeautifulSoup) -> str:
+        '''
+        Parses anime name from the card item.
+        '''
+        item_name = item.find(class_='card-title')
+        item_anime_link = item_name.find('a')
+        title_name = item_anime_link.get('title')
+        return title_name
+        
+    def _parse_link_from_card(self, item: BeautifulSoup) -> AnyHttpUrl:
+        '''
+        Parses anime link from the card item.
+        '''
+        item_name = item.find(class_='card-title')
+        item_anime_link = item_name.find('a')
+        title_link = item_anime_link.get('href')
+        return title_link
+        
+    def _parse_type_from_card(self, item: BeautifulSoup) -> AnimeType:
+        '''
+        Parses anime type from the card item.
+        '''
+        item_type_year = item.find(class_='animes-grid-item-body-info')
+        item_type = item_type_year.find(class_='text-link-gray').get('href')
+        title_type = item_type.split('/')[-1]
+        return self._types_compatibility[title_type]
+        
+    def _parse_year_from_card(self, item: BeautifulSoup) -> int:
+        '''
+        Parses anime year from the card item.
+        '''
+        item_type_year = item.find(class_='animes-grid-item-body-info')
+        item_year = item_type_year.find(class_='anime-year').find('a').get('href')
+        title_year = item_year.split('/')[-1]
+        return title_year
+
+    def parse_all_titles_count(self, web_page: WebPage) -> int:
+        '''Getts a number with the total count of titles.'''
+        self._logger.info("Parsing titles total count...")
+        
+        soup = BeautifulSoup(web_page, 'lxml')
+        try:
+            item_lists_table = soup.find(class_="card-header")
+        
+            item_list_all = None
+            items_lists = item_lists_table.find_all('a')
+            for item_list in items_lists:
+                if self._list_all_tag in item_list.text:
+                    item_list_all = item_list
+                    
+            total_text_item = item_list_all.find('span')
+            titles_count = total_text_item.text.strip()
+            titles_count = int(titles_count)
+            
+        except:
+            titles_count = 0
+        
+        self._logger.success("...parsed.\n")
+        return titles_count
+
+    def get_typed_anime_list(self, web_page: WebPage) -> typ.Dict[str, AnyHttpUrl]:
+        '''Gets an anime list by the type of watchlist. '''
+        pass
+
+    def get_anime_info(self, web_page: WebPage) -> LinkedAnimeInfoType:
+        '''Returns the anime data by the keys. '''
+        pass
 #--Finish functional block
